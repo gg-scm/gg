4435501ef13cb78e65a126f76e6065d197377c36
Ross Light <ross@zombiezen.com>
2018-03-08 19:06:11 -0800

    travis: add git build step
    Addresses #2

---
+++ misc/build-git.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir -p "$1" || exit 1
+cd "$1" || exit 1
+curl -fsSL https://www.kernel.org/pub/software/scm/git/git-"$2".tar.xz | xz -d | tar xf - --strip-components=1 || exit 1
+make -j2 git || exit 1
