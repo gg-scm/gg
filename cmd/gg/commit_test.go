61133bd021e56410ebce8c3a6f676a057a221d79
Ross Light <light@google.com>
2018-03-28 08:37:43 -0700

    cmd/gg: add test for passing arguments to commit

--- cmd/gg/commit_test.go
+++ cmd/gg/commit_test.go
@@ -31,7 +31,7 @@ const (
 	commitModifiedFileOldContent = "The history and lore\n"
 )
 
-func TestCommit(t *testing.T) {
+func TestCommit_NoArgs(t *testing.T) {
 	ctx := context.Background()
 	env, err := newTestEnv(ctx, t)
 	if err != nil {
@@ -70,6 +70,51 @@ func TestCommit(t *testing.T) {
 	}
 	if err := objectExists(ctx, env.git, r2.CommitHex()+":deleted.txt"); err == nil {
 		t.Error("deleted.txt exists")
+	}
+	if msg, err := readCommitMessage(ctx, env.git, r2.CommitHex()); err != nil {
+		t.Error(err)
+	} else if got := strings.TrimRight(string(msg), "\n"); got != wantMessage {
+		t.Errorf("commit message = %q; want %q", got, wantMessage)
+	}
+}
+
+func TestCommit_Selective(t *testing.T) {
+	ctx := context.Background()
+	env, err := newTestEnv(ctx, t)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := stageCommitTest(ctx, env); err != nil {
+		t.Fatal(err)
+	}
+	r1, err := gittool.ParseRev(ctx, env.git, "HEAD")
+	if err != nil {
+		t.Fatal(err)
+	}
+	const wantMessage = "gg made this commit"
+	if err := env.gg(ctx, env.root, "commit", "-m", wantMessage, "modified.txt"); err != nil {
+		t.Fatal(err)
+	}
+	r2, err := gittool.ParseRev(ctx, env.git, "HEAD")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if r1.CommitHex() == r2.CommitHex() {
+		t.Fatal("commit did not create a new commit in the working copy")
+	}
+	if ref := r2.RefName(); ref != "refs/heads/master" {
+		t.Fatalf("HEAD ref = %q; want refs/heads/master", ref)
+	}
+	if data, err := catBlob(ctx, env.git, r2.CommitHex(), "modified.txt"); err != nil {
+		t.Error(err)
+	} else if string(data) != commitModifiedFileContent {
+		t.Errorf("modified.txt = %q; want %q", data, commitModifiedFileContent)
+	}
+	if err := objectExists(ctx, env.git, r2.CommitHex()+":added.txt"); err == nil {
+		t.Error("added.txt was added but not put in arguments")
+	}
+	if err := objectExists(ctx, env.git, r2.CommitHex()+":deleted.txt"); err != nil {
+		t.Error("deleted.txt was removed but not put in arguments:", err)
 	}
 	if msg, err := readCommitMessage(ctx, env.git, r2.CommitHex()); err != nil {
 		t.Error(err)
@@ -213,5 +258,8 @@ func readCommitMessage(ctx context.Context, git *gittool.Tool, rev string) ([]by
 }
 
 func objectExists(ctx context.Context, git *gittool.Tool, obj string) error {
-	return git.Run(ctx, "cat-file", "-e", obj)
+	if err := git.Run(ctx, "cat-file", "-e", obj); err != nil {
+		return fmt.Errorf("object %s does not exist: %v", obj, err)
+	}
+	return nil
 }
