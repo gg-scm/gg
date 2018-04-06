0360883db2a538db71b4db2dc7619e6b1c971cf5
Ross Light <light@google.com>
2018-04-06 16:27:24 -0700

    travis: use bin-wrappers/git for test
    Build was failing because `git rebase` is a script, not a built-in, so
    GIT_EXEC_PATH wasn't set.  bin-wrappers/git is the provided way of
    running git in-place.

--- misc/build-git.bash
+++ misc/build-git.bash
@@ -18,4 +18,4 @@ set -o pipefail
 mkdir -p "$1" || exit 1
 cd "$1" || exit 1
 curl -fsSL https://www.kernel.org/pub/software/scm/git/git-"$2".tar.xz | xz -d | tar xf - --strip-components=1 || exit 1
-NO_GETTEXT=1 make -j2 git || exit 1
+NO_GETTEXT=1 make -j2 git bin-wrappers/git || exit 1
