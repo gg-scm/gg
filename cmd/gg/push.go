d4c0373ca6deb6dbed05b68b0f4c8f76f74dcd22
Ross Light <light@google.com>
2018-05-30 16:48:44 -0700

    cmd/gg: check remotes before trying to get push URL
    Fixes #36

--- cmd/gg/push.go
+++ cmd/gg/push.go
@@ -292,9 +292,12 @@ func escapeGerritMessage(sb *strings.Builder, msg string) {
 // remote. remote may either be a URL or the name of a remote, in
 // which case the remote's push URL will be queried.
 func verifyPushRemoteRef(ctx context.Context, git *gittool.Tool, remote string, ref gitobj.Ref) error {
-	pushURL, err := git.RunOneLiner(ctx, '\n', "remote", "get-url", "--push", "--", remote)
-	if err == nil {
-		// Errors typically mean that the argument is already a URL.
+	remotes, _ := listRemotes(ctx, git)
+	if _, isRemote := remotes[remote]; isRemote {
+		pushURL, err := git.RunOneLiner(ctx, '\n', "remote", "get-url", "--push", "--", remote)
+		if err != nil {
+			return err
+		}
 		remote = string(pushURL)
 	}
 	p, err := git.Start(ctx, "ls-remote", "--quiet", remote, ref.String())
