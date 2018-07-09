gg-scm/gg

commit 90693669e9bb82929637320fc40f4b0e4d34e027

internal/gittool: add StatusReader type
This was long overdue. Too many places replicated the status-read loop,
which is hard to get correct in the face of errors. This is especially
concerning in tests, which should be depending only on well-tested
functions. While the delta in this commit is large, the call sites are
much simpler to grok and less boilerplatey.

The new StatusReader wraps the same readStatusEntry function with a
bufio.Scanner-like API. The critical part is that calling Close() will
correctly send a kill signal to the subprocess and wait for it.
SignalReader suppresses EOFs and killed process errors so that errors
are more meaningful.

cmd/gg/status.go
@@ -15,15 +15,13 @@
 package main
 
 import (
-	"bufio"
 	"context"
 	"errors"
 	"fmt"
-	"io"
-	"strings"
 
 	"zombiezen.com/go/gg/internal/flag"
 	"zombiezen.com/go/gg/internal/gittool"
+	"zombiezen.com/go/gg/internal/singleclose"
 	"zombiezen.com/go/gg/internal/terminal"
 )
 
@@ -44,7 +42,6 @@ aliases: st, check`)
 		modifiedColor  []byte
 		removedColor   []byte
 		missingColor   []byte
-		ignoredColor   []byte
 		untrackedColor []byte
 		unmergedColor  []byte
 	)
@@ -76,43 +73,34 @@ aliases: st, check`)
 		if err != nil {
 			fmt.Fprintln(cc.stderr, "gg:", err)
 		}
-		ignoredColor, err = cfg.Color("color.ggstatus.ignored", "black")
-		if err != nil {
-			fmt.Fprintln(cc.stderr, "gg:", err)
-		}
 		unmergedColor, err = cfg.Color("color.ggstatus.unmerged", "blue")
 		if err != nil {
 			fmt.Fprintln(cc.stderr, "gg:", err)
 		}
 	}
-	p, err := cc.git.Start(ctx, append([]string{"status", "--porcelain", "-z", "-unormal", "--"}, f.Args()...)...)
+	st, err := gittool.Status(ctx, cc.git, f.Args())
 	if err != nil {
 		return err
 	}
-	defer p.Wait()
-	r := bufio.NewReader(p)
+	stClose := singleclose.For(st)
+	defer stClose.Close()
 	if colorize {
 		if err := terminal.ResetTextStyle(cc.stdout); err != nil {
 			return err
 		}
 	}
-	for {
-		ent, err := readStatusEntry(r)
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			return err
-		}
+	foundUnrecognized := false
+	for st.Scan() {
+		ent := st.Entry()
 		switch {
-		case ent.isModified():
-			_, err = fmt.Fprintf(cc.stdout, "%sM %s\n", modifiedColor, ent.name)
-		case ent.isAdded():
-			_, err = fmt.Fprintf(cc.stdout, "%sA %s\n", addedColor, ent.name)
-		case ent.isRemoved():
-			_, err = fmt.Fprintf(cc.stdout, "%sR %s\n", removedColor, ent.name)
-		case ent.isCopied():
-			if _, err := fmt.Fprintf(cc.stdout, "%sA %s\n", addedColor, ent.name); err != nil {
+		case ent.Code().IsModified():
+			_, err = fmt.Fprintf(cc.stdout, "%sM %s\n", modifiedColor, ent.Name())
+		case ent.Code().IsAdded():
+			_, err = fmt.Fprintf(cc.stdout, "%sA %s\n", addedColor, ent.Name())
+		case ent.Code().IsRemoved():
+			_, err = fmt.Fprintf(cc.stdout, "%sR %s\n", removedColor, ent.Name())
+		case ent.Code().IsCopied():
+			if _, err := fmt.Fprintf(cc.stdout, "%sA %s\n", addedColor, ent.Name()); err != nil {
 				return err
 			}
 			if colorize {
@@ -120,25 +108,24 @@ aliases: st, check`)
 					return err
 				}
 			}
-			_, err = fmt.Fprintf(cc.stdout, "  %s\n", ent.from)
-		case ent.isRenamed():
-			fmt.Fprintf(cc.stdout, "%sA %s\n", addedColor, ent.name)
+			_, err = fmt.Fprintf(cc.stdout, "  %s\n", ent.From())
+		case ent.Code().IsRenamed():
+			fmt.Fprintf(cc.stdout, "%sA %s\n", addedColor, ent.Name())
 			if colorize {
 				if err := terminal.ResetTextStyle(cc.stdout); err != nil {
 					return err
 				}
 			}
-			_, err = fmt.Fprintf(cc.stdout, "  %s\n%sR %s\n", ent.from, removedColor, ent.from)
-		case ent.isMissing():
-			_, err = fmt.Fprintf(cc.stdout, "%s! %s\n", missingColor, ent.name)
-		case ent.isUntracked():
-			_, err = fmt.Fprintf(cc.stdout, "%s? %s\n", untrackedColor, ent.name)
-		case ent.isIgnored():
-			_, err = fmt.Fprintf(cc.stdout, "%sI %s\n", ignoredColor, ent.name)
-		case ent.isUnmerged():
-			_, err = fmt.Fprintf(cc.stdout, "%sU %s\n", unmergedColor, ent.name)
+			_, err = fmt.Fprintf(cc.stdout, "  %s\n%sR %s\n", ent.From(), removedColor, ent.From())
+		case ent.Code().IsMissing():
+			_, err = fmt.Fprintf(cc.stdout, "%s! %s\n", missingColor, ent.Name())
+		case ent.Code().IsUntracked():
+			_, err = fmt.Fprintf(cc.stdout, "%s? %s\n", untrackedColor, ent.Name())
+		case ent.Code().IsUnmerged():
+			_, err = fmt.Fprintf(cc.stdout, "%sU %s\n", unmergedColor, ent.Name())
 		default:
-			panic("unreachable")
+			fmt.Fprintf(cc.stderr, "gg: unrecognized status for %s: '%v'\n", ent.Name(), ent.Code())
+			foundUnrecognized = true
 		}
 		if err != nil {
 			return err
@@ -149,139 +136,14 @@ aliases: st, check`)
 			}
 		}
 	}
-	return p.Wait()
-}
-
-type statusEntry struct {
-	code [2]byte
-	name string
-	from string
-}
-
-func (ent *statusEntry) isMissing() bool {
-	return ent.code[1] == 'D'
-}
-
-func (ent *statusEntry) isModified() bool {
-	return ent.code[0] == 'M' && ent.code[1] == ' ' ||
-		ent.code[0] == ' ' && ent.code[1] == 'M' ||
-		ent.code[0] == 'M' && ent.code[1] == 'M'
-}
-
-func (ent *statusEntry) isRemoved() bool {
-	return ent.code[0] == 'D' && ent.code[1] == ' '
-}
-
-func (ent *statusEntry) isRenamed() bool {
-	return ent.code[0] == 'R' && (ent.code[1] == ' ' || ent.code[1] == 'M')
-}
-
-func (ent *statusEntry) isCopied() bool {
-	return ent.code[0] == 'C' && (ent.code[1] == ' ' || ent.code[1] == 'M')
-}
-
-func (ent *statusEntry) isAdded() bool {
-	return ent.code[0] == 'A' && (ent.code[1] == ' ' || ent.code[1] == 'M') ||
-		ent.code[0] == ' ' && ent.code[1] == 'A'
-}
-
-func (ent *statusEntry) isUntracked() bool {
-	return ent.code[0] == '?' && ent.code[1] == '?'
-}
-
-func (ent *statusEntry) isIgnored() bool {
-	return ent.code[0] == '!' && ent.code[1] == '!'
-}
-
-func (ent *statusEntry) isUnmerged() bool {
-	return ent.code[0] == 'D' && ent.code[1] == 'D' ||
-		ent.code[0] == 'A' && ent.code[1] == 'U' ||
-		ent.code[0] == 'U' && ent.code[1] == 'D' ||
-		ent.code[0] == 'U' && ent.code[1] == 'A' ||
-		ent.code[0] == 'D' && ent.code[1] == 'U' ||
-		ent.code[0] == 'A' && ent.code[1] == 'A' ||
-		ent.code[0] == 'U' && ent.code[1] == 'U'
-}
-
-func readStatusEntry(r io.ByteReader) (*statusEntry, error) {
-	ent := new(statusEntry)
-	var err error
-	ent.code[0], err = r.ReadByte()
-	if err != nil {
-		return nil, err
+	if err := st.Err(); err != nil {
+		return err
 	}
-	ent.code[1], err = r.ReadByte()
-	if err == io.EOF {
-		return nil, errors.New("read status entry: unexpected EOF")
-	} else if err != nil {
-		return nil, fmt.Errorf("read status entry: %v", err)
+	if err := st.Close(); err != nil {
+		return err
 	}
-	if sp, err := r.ReadByte(); err == io.EOF {
-		return nil, errors.New("read status entry: unexpected EOF")
-	} else if err != nil {
-		return nil, fmt.Errorf("read status entry: %v", err)
-	} else if sp != ' ' {
-		return nil, fmt.Errorf("read status entry: expected ' ', got %q", sp)
+	if foundUnrecognized {
+		return errors.New("unrecognized output from git status. Please file a bug at https://github.com/zombiezen/gg/issues/new and include the output from this command.")
 	}
-	ent.name, err = readString(r, 2048)
-	if err != nil {
-		return nil, fmt.Errorf("read status entry: %v", err)
-	}
-	if ent.code[0] == 'R' || ent.code[0] == 'C' {
-		ent.from, err = readString(r, 2048)
-		if err != nil {
-			return nil, fmt.Errorf("read status entry: %v", err)
-		}
-	}
-	// Check at very end in order to consume as much as possible.
-	if !isValidStatusCode(ent.code) {
-		return nil, fmt.Errorf("read status entry: invalid code %q %q", ent.code[0], ent.code[1])
-	}
-	return ent, nil
-}
-
-func isValidStatusCode(code [2]byte) bool {
-	const codes = "??!!" +
-		" M D A" +
-		"M MMMD" +
-		"A AMAD" +
-		"D " +
-		"R RMRD" +
-		"C CMCD" +
-		"DDAUUDUADUAAUU"
-	for i := 0; i < len(codes); i += 2 {
-		if code[0] == codes[i] && code[1] == codes[i+1] {
-			return true
-		}
-	}
-	return false
-}
-
-// readString reads a NUL-terminated string from r.
-func readString(r io.ByteReader, limit int) (string, error) {
-	var sb strings.Builder
-	for sb.Len() < limit {
-		b, err := r.ReadByte()
-		if err == io.EOF {
-			return "", io.ErrUnexpectedEOF
-		}
-		if err != nil {
-			return "", err
-		}
-		if b == 0 {
-			return sb.String(), nil
-		}
-		sb.WriteByte(b)
-	}
-	b, err := r.ReadByte()
-	if err == io.EOF {
-		return "", io.ErrUnexpectedEOF
-	}
-	if err != nil {
-		return "", err
-	}
-	if b != 0 {
-		return "", errors.New("string too long")
-	}
-	return sb.String(), nil
+	return nil
 }

internal/gittool/status.go
@@ -1,0 +1,294 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gittool
+
+import (
+	"bufio"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"strings"
+)
+
+// StatusReader is a handle to a running `git status` command.
+// It does not return clean or ignored files.
+type StatusReader struct {
+	p      *Process
+	r      *bufio.Reader
+	cancel context.CancelFunc
+
+	scanned bool
+	ent     StatusEntry
+	err     error
+}
+
+// Status starts a `git status` subprocess.
+func Status(ctx context.Context, git *Tool, args []string) (*StatusReader, error) {
+	ctx, cancel := context.WithCancel(ctx)
+	gitArgs := make([]string, 0, 5+len(args))
+	gitArgs = append(gitArgs, "status", "--porcelain", "-z", "-unormal", "--")
+	gitArgs = append(gitArgs, args...)
+	p, err := git.Start(ctx, gitArgs...)
+	if err != nil {
+		return nil, err
+	}
+	return &StatusReader{
+		p:      p,
+		r:      bufio.NewReader(p),
+		cancel: cancel,
+	}, nil
+}
+
+// Scan reads the next entry in the status output.
+func (sr *StatusReader) Scan() bool {
+	sr.err = readStatusEntry(&sr.ent, sr.r)
+	if sr.err != nil {
+		return false
+	}
+	sr.scanned = true
+	return true
+}
+
+// Err returns the first non-EOF error encountered during Scan.
+func (sr *StatusReader) Err() error {
+	if sr.err == io.EOF {
+		return nil
+	}
+	return sr.err
+}
+
+// Entry returns the most recent entry parsed by a call to Scan.
+// The pointer may point to data that will be overwritten by a
+// subsequent call to Scan.
+func (sr *StatusReader) Entry() *StatusEntry {
+	if !sr.scanned || sr.err != nil {
+		return nil
+	}
+	return &sr.ent
+}
+
+// Close finishes reading from the Git subprocess and waits for it to
+// terminate. The behavior of calling methods on a StatusReader after
+// Close is undefined.
+//
+// If the subprocess exited due to a signal, Close will not return an
+// error, as it usually means that Close terminated the process. In the
+// case that another signal terminated the subprocess, this usually
+// results in a scan error.
+func (sr *StatusReader) Close() error {
+	sr.cancel()
+	err := sr.p.Wait()
+	*sr = StatusReader{}
+	switch err := err.(type) {
+	case nil:
+		return nil
+	case *exitError:
+		if err.signaled {
+			return nil
+		}
+		return err
+	default:
+		return err
+	}
+}
+
+// A StatusEntry describes the state of a single file in the working copy.
+type StatusEntry struct {
+	code StatusCode
+	name string
+	from string
+}
+
+func readStatusEntry(out *StatusEntry, r io.ByteReader) error {
+	var err error
+	// Read status code.
+	out.code[0], err = r.ReadByte()
+	if err == io.EOF {
+		return err
+	}
+	if err != nil {
+		return fmt.Errorf("read status entry: %v", err)
+	}
+	out.code[1], err = r.ReadByte()
+	if err != nil {
+		return fmt.Errorf("read status entry: %v", dontExpectEOF(err))
+	}
+
+	// Read space.
+	sp, err := r.ReadByte()
+	if err != nil {
+		return fmt.Errorf("read status entry: %v", dontExpectEOF(err))
+	}
+	if sp != ' ' {
+		return fmt.Errorf("read status entry: expected ' ', got %q", sp)
+	}
+
+	// Read name and from.
+	out.name, err = readString(r, 2048)
+	if err != nil {
+		return fmt.Errorf("read status entry: %v", err)
+	}
+	if out.code[0] == 'R' || out.code[0] == 'C' {
+		out.from, err = readString(r, 2048)
+		if err != nil {
+			return fmt.Errorf("read status entry: %v", err)
+		}
+	}
+
+	// Check code validity at very end in order to consume as much as possible.
+	if !out.code.isValid() {
+		return fmt.Errorf("read status entry: invalid code %q %q", out.code[0], out.code[1])
+	}
+	return nil
+}
+
+// readString reads a NUL-terminated string from r.
+func readString(r io.ByteReader, limit int) (string, error) {
+	var sb strings.Builder
+	for sb.Len() < limit {
+		b, err := r.ReadByte()
+		if err != nil {
+			return "", dontExpectEOF(err)
+		}
+		if b == 0 {
+			return sb.String(), nil
+		}
+		sb.WriteByte(b)
+	}
+	b, err := r.ReadByte()
+	if err != nil {
+		return "", dontExpectEOF(err)
+	}
+	if b != 0 {
+		return "", errors.New("string too long")
+	}
+	return sb.String(), nil
+}
+
+// String returns the entry in short format.
+func (ent *StatusEntry) String() string {
+	if ent.from != "" {
+		return ent.code.String() + " " + ent.from + " -> " + ent.name
+	}
+	return ent.code.String() + " " + ent.name
+}
+
+// Code returns the two-letter code from the git status short format.
+//
+// More details in the Output section of git-status(1).
+func (ent *StatusEntry) Code() StatusCode {
+	return ent.code
+}
+
+// Name returns the path of the file. The path will always be relative
+// to the top of the repository.
+func (ent *StatusEntry) Name() string {
+	return ent.name
+}
+
+// From returns the path of the file that this file was renamed or
+// copied from, otherwise an empty string. The path will always be
+/// relative to the top of the repository.
+func (ent *StatusEntry) From() string {
+	return ent.from
+}
+
+// A StatusCode is a two-letter code from the `git status` short format.
+// For paths with no merge conflicts, the first letter is the status of
+// the index and the second letter is the status of the work tree.
+//
+// More details at https://git-scm.com/docs/git-status#_short_format
+type StatusCode [2]byte
+
+// String returns the code's bytes as a string.
+func (code StatusCode) String() string {
+	return string(code[:])
+}
+
+// IsMissing reports whether the file has been deleted in the work tree.
+func (code StatusCode) IsMissing() bool {
+	return code[1] == 'D'
+}
+
+// IsModified reports whether the file has been modified in either the
+// index or the work tree.
+func (code StatusCode) IsModified() bool {
+	return code[0] == 'M' && code[1] == ' ' ||
+		code[0] == ' ' && code[1] == 'M' ||
+		code[0] == 'M' && code[1] == 'M'
+}
+
+// IsRemoved reports whether the file has been deleted in the index.
+func (code StatusCode) IsRemoved() bool {
+	return code[0] == 'D' && code[1] == ' '
+}
+
+// IsRenamed reports whether the file has been renamed.
+func (code StatusCode) IsRenamed() bool {
+	return code[0] == 'R' && (code[1] == ' ' || code[1] == 'M')
+}
+
+// IsCopied reports whether the file has been copied.
+func (code StatusCode) IsCopied() bool {
+	return code[0] == 'C' && (code[1] == ' ' || code[1] == 'M')
+}
+
+// IsAdded reports whether the file has been added in either the index
+// or the work tree.
+func (code StatusCode) IsAdded() bool {
+	return code[0] == 'A' && (code[1] == ' ' || code[1] == 'M') ||
+		code[0] == ' ' && code[1] == 'A'
+}
+
+// IsUntracked returns true if the file is not being tracked by Git.
+func (code StatusCode) IsUntracked() bool {
+	return code[0] == '?' && code[1] == '?'
+}
+
+// IsUnmerged reports whether the file has unresolved merge conflicts.
+func (code StatusCode) IsUnmerged() bool {
+	return code[0] == 'D' && code[1] == 'D' ||
+		code[0] == 'A' && code[1] == 'U' ||
+		code[0] == 'U' && code[1] == 'D' ||
+		code[0] == 'U' && code[1] == 'A' ||
+		code[0] == 'D' && code[1] == 'U' ||
+		code[0] == 'A' && code[1] == 'A' ||
+		code[0] == 'U' && code[1] == 'U'
+}
+
+func (code StatusCode) isValid() bool {
+	const codes = "??!!" +
+		" M D A" +
+		"M MMMD" +
+		"A AMAD" +
+		"D " +
+		"R RMRD" +
+		"C CMCD" +
+		"DDAUUDUADUAAUU"
+	for i := 0; i < len(codes); i += 2 {
+		if code[0] == codes[i] && code[1] == codes[i+1] {
+			return true
+		}
+	}
+	return false
+}
+
+func dontExpectEOF(e error) error {
+	if e == io.EOF {
+		return io.ErrUnexpectedEOF
+	}
+	return e
+}

internal/gittool/status_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main
+package gittool
 
 import (
 	"io"
@@ -22,9 +22,12 @@ import (
 
 func TestReadStatusEntry(t *testing.T) {
 	tests := []struct {
-		name      string
-		data      string
-		ent       statusEntry
+		name string
+		data string
+
+		code      StatusCode
+		entName   string
+		from      string
 		err       func(error) bool
 		remaining string
 	}{
@@ -34,12 +37,10 @@ func TestReadStatusEntry(t *testing.T) {
 			err:  func(e error) bool { return e == io.EOF },
 		},
 		{
-			name: "ModifiedWorkTree",
-			data: " M foo.txt\x00",
-			ent: statusEntry{
-				code: [2]byte{' ', 'M'},
-				name: "foo.txt",
-			},
+			name:    "ModifiedWorkTree",
+			data:    " M foo.txt\x00",
+			code:    StatusCode{' ', 'M'},
+			entName: "foo.txt",
 		},
 		{
 			name: "MissingNul",
@@ -47,37 +48,32 @@ func TestReadStatusEntry(t *testing.T) {
 			err:  func(e error) bool { return e != nil && e != io.EOF },
 		},
 		{
-			name: "ModifiedIndex",
-			data: "MM foo.txt\x00",
-			ent: statusEntry{
-				code: [2]byte{'M', 'M'},
-				name: "foo.txt",
-			},
+			name:    "ModifiedIndex",
+			data:    "MM foo.txt\x00",
+			code:    StatusCode{'M', 'M'},
+			entName: "foo.txt",
 		},
 		{
-			name: "Renamed",
-			data: "R  bar.txt\x00foo.txt\x00",
-			ent: statusEntry{
-				code: [2]byte{'R', ' '},
-				name: "bar.txt",
-				from: "foo.txt",
-			},
+			name:    "Renamed",
+			data:    "R  bar.txt\x00foo.txt\x00",
+			code:    StatusCode{'R', ' '},
+			entName: "bar.txt",
+			from:    "foo.txt",
 		},
 		{
-			name: "Multiple",
-			data: "R  bar.txt\x00foo.txt\x00MM baz.txt\x00",
-			ent: statusEntry{
-				code: [2]byte{'R', ' '},
-				name: "bar.txt",
-				from: "foo.txt",
-			},
+			name:      "Multiple",
+			data:      "R  bar.txt\x00foo.txt\x00MM baz.txt\x00",
+			code:      StatusCode{'R', ' '},
+			entName:   "bar.txt",
+			from:      "foo.txt",
 			remaining: "MM baz.txt\x00",
 		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			r := strings.NewReader(test.data)
-			ent, err := readStatusEntry(r)
+			var ent StatusEntry
+			err := readStatusEntry(&ent, r)
 			if remaining := test.data[len(test.data)-r.Len():]; remaining != test.remaining {
 				t.Errorf("after readStatusEntry, remaining = %q; want %q", remaining, test.remaining)
 			}
@@ -93,8 +89,14 @@ func TestReadStatusEntry(t *testing.T) {
 			if test.err != nil {
 				t.Fatal("readStatusEntry(...) = _, <nil>; want error")
 			}
-			if ent.code != test.ent.code || ent.name != test.ent.name || ent.from != test.ent.from {
-				t.Fatalf("readStatusEntry(...) = %#v; want %#v", ent, test.ent)
+			if got, want := ent.Code(), test.code; got != want {
+				t.Errorf("readStatusEntry(...).Code() = '%v'; want '%v'", got, want)
+			}
+			if got, want := ent.Name(), test.entName; got != want {
+				t.Errorf("readStatusEntry(...).Name() = %q; want %q", got, want)
+			}
+			if got, want := ent.From(), test.from; got != want {
+				t.Errorf("readStatusEntry(...).From() = %q; want %q", got, want)
 			}
 		})
 	}

cmd/gg/add.go
@@ -15,12 +15,11 @@
 package main
 
 import (
-	"bufio"
 	"context"
-	"io"
 
 	"zombiezen.com/go/gg/internal/flag"
 	"zombiezen.com/go/gg/internal/gittool"
+	"zombiezen.com/go/gg/internal/singleclose"
 )
 
 const addSynopsis = "add the specified files on the next commit"
@@ -61,32 +60,29 @@ func add(ctx context.Context, cc *cmdContext, args []string) error {
 // splitUnmerged finds the files described by the arguments and groups
 // them into normal files and unmerged files.
 func splitUnmerged(ctx context.Context, git *gittool.Tool, args []string) (normal, unmerged []string, _ error) {
-	statusArgs := []string{"status", "--porcelain", "-z", "-unormal", "--"}
-	for _, a := range args {
-		statusArgs = append(statusArgs, ":(literal)"+a)
+	statusArgs := make([]string, len(args))
+	for i := range args {
+		statusArgs[i] = ":(literal)" + args[i]
 	}
-	p, err := git.Start(ctx, statusArgs...)
+	st, err := gittool.Status(ctx, git, statusArgs)
 	if err != nil {
 		return nil, nil, err
 	}
-	defer p.Wait()
-	r := bufio.NewReader(p)
-	for {
-		ent, err := readStatusEntry(r)
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			if waitErr := p.Wait(); waitErr != nil {
-				return nil, nil, waitErr
-			}
-			return nil, nil, err
-		}
-		if ent.isUnmerged() {
-			unmerged = append(unmerged, ":(top,literal)"+ent.name)
+	stClose := singleclose.For(st)
+	defer stClose.Close()
+	for st.Scan() {
+		ent := st.Entry()
+		if ent.Code().IsUnmerged() {
+			unmerged = append(unmerged, ":(top,literal)"+ent.Name())
 		} else {
-			normal = append(normal, ":(top,literal)"+ent.name)
+			normal = append(normal, ":(top,literal)"+ent.Name())
 		}
 	}
-	return normal, unmerged, p.Wait()
+	if err := st.Err(); err != nil {
+		return nil, nil, err
+	}
+	if err := stClose.Close(); err != nil {
+		return nil, nil, err
+	}
+	return normal, unmerged, nil
 }

commit 55b537a6701e869cd007ea9c554a49abab8c2baf

cmd/gg: integration test for status command
Found breakage introduced by 90693669e9bb82929637320fc40f4b0e4d34e027.

cmd/gg/status.go
@@ -139,7 +139,7 @@ aliases: st, check`)
 	if err := st.Err(); err != nil {
 		return err
 	}
-	if err := st.Close(); err != nil {
+	if err := stClose.Close(); err != nil {
 		return err
 	}
 	if foundUnrecognized {

cmd/gg/status_test.go
@@ -1,0 +1,99 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"bytes"
+	"context"
+	"testing"
+)
+
+func TestStatus(t *testing.T) {
+	ctx := context.Background()
+	env, err := newTestEnv(ctx, t)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer env.cleanup()
+	if err := stageCommitTest(ctx, env, true); err != nil {
+		t.Fatal(err)
+	}
+
+	out, err := env.gg(ctx, env.root, "status")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("Output:\n%s", out)
+	foundAdded, foundModified, foundDeleted := false, false, false
+	for lineno := 1; len(out) > 0; lineno++ {
+		var line []byte
+		line, out = splitLine(out)
+		if len(line) < 2 {
+			t.Errorf("Line %d: got %q; want >3 characters for status, then space, then name", lineno, line)
+			continue
+		}
+		if line[1] != ' ' {
+			t.Errorf("Line %d: got %q; want second character to be a space", lineno, line)
+		}
+		switch name := string(line[2:]); name {
+		case "added.txt":
+			if foundAdded {
+				t.Error("Duplicate for added.txt")
+				continue
+			}
+			foundAdded = true
+			if line[0] != 'A' {
+				t.Errorf("Line %d: added.txt status = %q; want 'A'", lineno, line[0])
+			}
+		case "modified.txt":
+			if foundModified {
+				t.Error("Duplicate for modified.txt")
+				continue
+			}
+			foundModified = true
+			if line[0] != 'M' {
+				t.Errorf("Line %d: modified.txt status = %q; want 'M'", lineno, line[0])
+			}
+		case "deleted.txt":
+			if foundDeleted {
+				t.Error("Duplicate for deleted.txt")
+				continue
+			}
+			foundDeleted = true
+			if line[0] != 'R' {
+				t.Errorf("Line %d: deleted.txt status = %q; want 'R'", lineno, line[0])
+			}
+		default:
+			t.Errorf("Line %d: got unexpected file %q", lineno, name)
+		}
+	}
+	if !foundAdded {
+		t.Error("No status line for added.txt")
+	}
+	if !foundModified {
+		t.Error("No status line for modified.txt")
+	}
+	if !foundDeleted {
+		t.Error("No status line for deleted.txt")
+	}
+}
+
+func splitLine(b []byte) (line, rest []byte) {
+	i := bytes.IndexByte(b, '\n')
+	if i == -1 {
+		return b, nil
+	}
+	return b[:i], b[i+1:]
+}