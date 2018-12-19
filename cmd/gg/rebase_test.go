8c972ff28d01dbe7c80521d7252339cbef25128d
Ross Light <light@google.com>
2018-12-19 07:59:50 -0800

    cmd/gg: fix broken rebase tests

--- cmd/gg/rebase_test.go
+++ cmd/gg/rebase_test.go
@@ -739,7 +739,7 @@ func TestHistedit_ContinueWithModifications(t *testing.T) {
 			t.Fatal(err)
 		}
 		const wantMessage2 = "Divergence 2\n"
-		if err := env.git.Commit(ctx, "Divergence 2"); err != nil {
+		if err := env.git.Commit(ctx, wantMessage2); err != nil {
 			t.Fatal(err)
 		}
 		rev2, err := env.git.Head(ctx)
@@ -888,7 +888,7 @@ func TestHistedit_ContinueNoModifications(t *testing.T) {
 			t.Fatal(err)
 		}
 		const wantMessage1 = "Divergence 1\n"
-		if err := env.git.Commit(ctx, "Divergence 1"); err != nil {
+		if err := env.git.Commit(ctx, wantMessage1); err != nil {
 			t.Fatal(err)
 		}
 		rev1, err := env.git.Head(ctx)
@@ -902,7 +902,7 @@ func TestHistedit_ContinueNoModifications(t *testing.T) {
 			t.Fatal(err)
 		}
 		const wantMessage2 = "Divergence 2\n"
-		if err := env.git.Commit(ctx, "Divergence 2"); err != nil {
+		if err := env.git.Commit(ctx, wantMessage2); err != nil {
 			t.Fatal(err)
 		}
 		rev2, err := env.git.Head(ctx)
