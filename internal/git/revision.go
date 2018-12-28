0d6b5a6cacaf504e0875c58f0cd603e042fd81f1
Ross Light <light@google.com>
2018-12-28 08:58:53 -0800

    internal/git: merge object.go and revision.go

--- internal/git/revision.go
+++ internal/git/revision.go
@@ -16,8 +16,100 @@ package git
 
 import (
 	"context"
+	"encoding/hex"
 	"fmt"
 	"strings"
+)
+
+// hashSize is the number of bytes in a hash.
+const hashSize = 20
+
+// A Hash is the SHA-1 hash of a Git object.
+type Hash [hashSize]byte
+
+// ParseHash parses a hex-encoded hash.
+func ParseHash(s string) (Hash, error) {
+	if len(s) != hex.EncodedLen(hashSize) {
+		return Hash{}, fmt.Errorf("parse hash %q: wrong size", s)
+	}
+	var h Hash
+	if _, err := hex.Decode(h[:], []byte(s)); err != nil {
+		return Hash{}, fmt.Errorf("parse hash %q: %v", s, err)
+	}
+	return h, nil
+}
+
+// String returns the hex-encoded hash.
+func (h Hash) String() string {
+	return hex.EncodeToString(h[:])
+}
+
+// Short returns the first 4 hex-encoded bytes of the hash.
+func (h Hash) Short() string {
+	return hex.EncodeToString(h[:4])
+}
+
+// A Ref is a Git reference to a commit.
+type Ref string
+
+// Top-level refs.
+const (
+	// Head names the commit on which the changes in the working tree
+	// are based.
+	Head Ref = "HEAD"
+
+	// FetchHead records the branch which was fetched from a remote
+	// repository with the last git fetch invocation.
+	FetchHead Ref = "FETCH_HEAD"
+)
+
+// BranchRef returns a ref for the given branch name.
+func BranchRef(b string) Ref {
+	return branchPrefix + Ref(b)
+}
+
+// IsValid reports whether r is a valid reference.
+func (r Ref) IsValid() bool {
+	return r != "" && r[0] != '-'
+}
+
+// String returns the ref as a string.
+func (r Ref) String() string {
+	return string(r)
+}
+
+// IsBranch reports whether r starts with "refs/heads/".
+func (r Ref) IsBranch() bool {
+	return strings.HasPrefix(string(r), branchPrefix)
+}
+
+// Branch returns the string after "refs/heads/" or an empty string
+// if the ref does not start with "refs/heads/".
+func (r Ref) Branch() string {
+	if !r.IsBranch() {
+		return ""
+	}
+	return string(r[len(branchPrefix):])
+}
+
+// IsTag reports whether r starts with "refs/tags/".
+func (r Ref) IsTag() bool {
+	return strings.HasPrefix(string(r), tagPrefix)
+}
+
+// Tag returns the string after "refs/tags/" or an empty string
+// if the ref does not start with "refs/tags/".
+func (r Ref) Tag() string {
+	if !r.IsTag() {
+		return ""
+	}
+	return string(r[len(tagPrefix):])
+}
+
+// Ref prefixes.
+const (
+	branchPrefix = "refs/heads/"
+	tagPrefix    = "refs/tags/"
 )
 
 // Head returns the working copy's branch revision.
--- internal/git/revision_test.go
+++ internal/git/revision_test.go
@@ -23,6 +23,140 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
+func TestHash(t *testing.T) {
+	tests := []struct {
+		h     Hash
+		s     string
+		short string
+	}{
+		{
+			h:     Hash{},
+			s:     "0000000000000000000000000000000000000000",
+			short: "00000000",
+		},
+		{
+			h: Hash{
+				0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef,
+				0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef,
+				0x01, 0x23, 0x45, 0x67,
+			},
+			s:     "0123456789abcdef0123456789abcdef01234567",
+			short: "01234567",
+		},
+	}
+	for _, test := range tests {
+		if got := test.h.String(); got != test.s {
+			t.Errorf("Hash(%x).String() = %q; want %q", test.h[:], got, test.s)
+		}
+		if got := test.h.Short(); got != test.short {
+			t.Errorf("Hash(%x).Short() = %q; want %q", test.h[:], got, test.short)
+		}
+	}
+}
+
+func TestParseHash(t *testing.T) {
+	tests := []struct {
+		s       string
+		want    Hash
+		wantErr bool
+	}{
+		{s: "", wantErr: true},
+		{s: "0000000000000000000000000000000000000000", want: Hash{}},
+		{
+			s: "0123456789abcdef0123456789abcdef01234567",
+			want: Hash{
+				0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef,
+				0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef,
+				0x01, 0x23, 0x45, 0x67,
+			},
+		},
+		{
+			s:       "0123456789abcdef0123456789abcdef0123456",
+			wantErr: true,
+		},
+		{
+			s:       "0123456789abcdef0123456789abcdef012345678",
+			wantErr: true,
+		},
+		{
+			s:       "01234567",
+			wantErr: true,
+		},
+	}
+	for _, test := range tests {
+		switch got, err := ParseHash(test.s); {
+		case err == nil && !test.wantErr && got != test.want:
+			t.Errorf("ParseHash(%q) = %v, <nil>; want %v, <nil>", test.s, got, test.want)
+		case err == nil && test.wantErr:
+			t.Errorf("ParseHash(%q) = %v, <nil>; want error", test.s, got)
+		case err != nil && !test.wantErr:
+			t.Errorf("ParseHash(%q) = _, %v; want %v, <nil>", test.s, err, test.want)
+		}
+	}
+}
+
+func TestRef(t *testing.T) {
+	tests := []struct {
+		ref      Ref
+		invalid  bool
+		isBranch bool
+		branch   string
+		isTag    bool
+		tag      string
+	}{
+		{
+			ref:     "",
+			invalid: true,
+		},
+		{
+			ref:     "-",
+			invalid: true,
+		},
+		{ref: "master"},
+		{ref: "HEAD"},
+		{ref: "FETCH_HEAD"},
+		{ref: "ORIG_HEAD"},
+		{ref: "MERGE_HEAD"},
+		{ref: "CHERRY_PICK_HEAD"},
+		{ref: "FOO"},
+		{
+			ref:     "-refs/heads/master",
+			invalid: true,
+		},
+		{
+			ref:      "refs/heads/master",
+			isBranch: true,
+			branch:   "master",
+		},
+		{
+			ref:   "refs/tags/v1.2.3",
+			isTag: true,
+			tag:   "v1.2.3",
+		},
+		{ref: "refs/for/master"},
+	}
+	for _, test := range tests {
+		if got := test.ref.String(); got != string(test.ref) {
+			t.Errorf("Ref(%q).String() = %q; want %q", string(test.ref), got, string(test.ref))
+		}
+		if got := test.ref.IsValid(); got != !test.invalid {
+			t.Errorf("Ref(%q).IsValid() = %t; want %t", string(test.ref), got, !test.invalid)
+		}
+		if got := test.ref.IsBranch(); got != test.isBranch {
+			t.Errorf("Ref(%q).IsBranch() = %t; want %t", string(test.ref), got, test.isBranch)
+		}
+		if got := test.ref.Branch(); got != test.branch {
+			t.Errorf("Ref(%q).Branch() = %s; want %s", string(test.ref), got, test.branch)
+		}
+		if got := test.ref.IsTag(); got != test.isTag {
+			t.Errorf("Ref(%q).IsTag() = %t; want %t", string(test.ref), got, test.isTag)
+		}
+		if got := test.ref.Tag(); got != test.tag {
+			t.Errorf("Ref(%q).Tag() = %s; want %s", string(test.ref), got, test.tag)
+		}
+	}
+}
+
 func TestParseRev(t *testing.T) {
 	if testing.Short() {
 		t.Skip("skipping due to -short")
