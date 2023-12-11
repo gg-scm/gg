af3ab0b8ddca5513f86812d820802202514ed986
Ross Light <ross@zombiezen.com>
2023-12-11 14:49:04 -0800

    internal/gitrepo: new package

---
+++ internal/gitrepo/gitrepo.go
@@ -0,0 +1,215 @@
+// Copyright 2023 The gg Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//		 https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package gitrepo
+
+import (
+	"bytes"
+	"context"
+	"crypto/sha1"
+	"fmt"
+	"io"
+	"math"
+
+	"gg-scm.io/pkg/git/githash"
+	"gg-scm.io/pkg/git/object"
+)
+
+// A type that implements Repository can retrieve Git objects.
+//
+// OpenObject returns a reader for the object with the given hash.
+// If the reader returned from OpenObject returns an EOF,
+// it guarantees that the bytes read match the hash.
+//
+// Stat returns the same information but without returning a reader.
+// This is equivalent to calling OpenObject and immediately calling Close on rc,
+// but implementations may have a more efficient implementation.
+type Repository interface {
+	OpenObject(ctx context.Context, id githash.SHA1) (object.Prefix, io.ReadCloser, error)
+	Stat(ctx context.Context, id githash.SHA1) (object.Prefix, error)
+}
+
+// A type that implements Catter can retrieve Git objects
+// and dereference objects to find the object of the correct type.
+type Catter interface {
+	Cat(ctx context.Context, dst io.Writer, tp object.Type, id githash.SHA1) error
+}
+
+// Cat copies the content of the given object from the cache into dst.
+// If the type of the object requested does not match the requested type
+// and it can be trivially dereferenced to the requested type
+// (e.g. a commit is found during a request for a tree),
+// then the referenced object is written to dst.
+//
+// The given buffer will be used to store intermediate objects:
+// it's assumed that reading from buf will read the bytes
+// previously written to buf.
+//
+// If cat implements [TypeCatter], it is used instead of reading individual objects.
+// buf will not be used in this case.
+func Cat(ctx context.Context, repo Repository, dst io.Writer, wantType object.Type, id githash.SHA1) error {
+	if typeCat, ok := repo.(Catter); ok {
+		return typeCat.Cat(ctx, dst, wantType, id)
+	}
+
+	var nextType object.Type
+	buf := new(bytes.Buffer)
+	for nextID := id; ; {
+		got, r, err := repo.OpenObject(ctx, nextID)
+		if err != nil {
+			return fmt.Errorf("cat %v %v: %v", wantType, id, err)
+		}
+		if got.Type == wantType {
+			_, err := io.Copy(dst, r)
+			r.Close()
+			if err != nil {
+				return fmt.Errorf("cat %v %v: %v", wantType, id, err)
+			}
+			return nil
+		}
+		if nextType != "" && got.Type != nextType {
+			return fmt.Errorf("cat %v %v: %v is a %v (expected %v)", wantType, id, nextID, got.Type, nextType)
+		}
+
+		switch {
+		case got.Type == object.TypeCommit && wantType == object.TypeTree:
+			_, err := io.Copy(buf, r)
+			r.Close()
+			if err != nil {
+				return fmt.Errorf("cat %v %v: %v", wantType, id, err)
+			}
+			c, err := object.ParseCommit(buf.Bytes())
+			if err != nil {
+				return fmt.Errorf("cat %v %v: %v", wantType, id, err)
+			}
+			buf.Reset()
+			nextID = c.Tree
+			nextType = object.TypeTree
+		case got.Type == object.TypeTag:
+			_, err := io.Copy(buf, r)
+			r.Close()
+			if err != nil {
+				return fmt.Errorf("cat %v %v: %v", wantType, id, err)
+			}
+			t, err := object.ParseTag(buf.Bytes())
+			if err != nil {
+				return fmt.Errorf("cat %v %v: %v", wantType, id, err)
+			}
+			buf.Reset()
+			nextID = t.ObjectID
+			nextType = t.ObjectType
+			if !(nextType == wantType || nextType == object.TypeCommit && wantType == object.TypeTree) {
+				return fmt.Errorf("cat %v %v: tag references a %v", wantType, id, nextType)
+			}
+		default:
+			r.Close()
+			return fmt.Errorf("cat %v %v: %v is a %v", wantType, id, nextID, got.Type)
+		}
+	}
+
+}
+
+// Map is an in-memory implementation of [Repository].
+// The zero value is an empty repository.
+type Map map[githash.SHA1]Object
+
+// OpenObject returns the object.
+func (m Map) OpenObject(ctx context.Context, id githash.SHA1) (object.Prefix, io.ReadCloser, error) {
+	obj, err := m.get(ctx, id)
+	if err != nil {
+		return object.Prefix{}, nil, err
+	}
+	return obj.Prefix(), io.NopCloser(bytes.NewReader(obj.Data)), nil
+}
+
+// Stat returns the object type and size of the given key.
+func (m Map) Stat(ctx context.Context, id githash.SHA1) (object.Prefix, error) {
+	obj, err := m.get(ctx, id)
+	if err != nil {
+		return object.Prefix{}, err
+	}
+	return obj.Prefix(), nil
+}
+
+func (m Map) get(ctx context.Context, id githash.SHA1) (Object, error) {
+	obj, ok := m[id]
+	if !ok {
+		return Object{}, fmt.Errorf("open %v: not found", id)
+	}
+	if id != obj.SHA1() {
+		return Object{}, fmt.Errorf("open %v: corrupted", id)
+	}
+	return obj, nil
+}
+
+// WriteObject adds an object to the map.
+func (m *Map) WriteObject(ctx context.Context, prefix object.Prefix, r io.Reader) (githash.SHA1, error) {
+	if !prefix.Type.IsValid() {
+		return githash.SHA1{}, fmt.Errorf("write git object: invalid type %q", prefix.Type)
+	}
+	if prefix.Size > math.MaxInt {
+		return githash.SHA1{}, fmt.Errorf("write %v: too large (%d bytes)", prefix.Type, prefix.Size)
+	}
+	if prefix.Size < 0 {
+		return githash.SHA1{}, fmt.Errorf("write %v: negative size", prefix.Type)
+	}
+	obj := Object{
+		Type: prefix.Type,
+		Data: make([]byte, prefix.Size),
+	}
+	if _, err := io.ReadFull(r, obj.Data); err != nil {
+		return githash.SHA1{}, fmt.Errorf("write %v: %v", prefix.Type, err)
+	}
+	return m.Add(obj), nil
+}
+
+// Add adds an object to the map.
+func (m *Map) Add(obj Object) githash.SHA1 {
+	if *m == nil {
+		*m = make(Map)
+	}
+	id := obj.SHA1()
+	(*m)[id] = obj
+	return id
+}
+
+// Object is an in-memory Git object.
+type Object struct {
+	Type object.Type
+	Data []byte
+}
+
+// Prefix returns the object's data.
+func (obj Object) Prefix() object.Prefix {
+	return object.Prefix{
+		Type: obj.Type,
+		Size: int64(len(obj.Data)),
+	}
+}
+
+// SHA1 returns the SHA-1 hash of the object.
+func (obj Object) SHA1() githash.SHA1 {
+	h := sha1.New()
+	prefix, err := obj.Prefix().MarshalBinary()
+	if err != nil {
+		panic(err)
+	}
+	h.Write(prefix)
+	h.Write(obj.Data)
+	var id githash.SHA1
+	h.Sum(id[:0])
+	return id
+}
---
+++ internal/gitrepo/gitrepo_test.go
@@ -0,0 +1,213 @@
+// Copyright 2023 The gg Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//		 https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package gitrepo
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"strings"
+	"testing"
+	"time"
+
+	"gg-scm.io/pkg/git/githash"
+	"gg-scm.io/pkg/git/object"
+	"github.com/google/go-cmp/cmp"
+)
+
+var _ Repository = Map(nil)
+
+func TestMap(t *testing.T) {
+	ctx := context.Background()
+	var repo Map
+	const content = "Hello, World!\n"
+	wantHash, err := githash.ParseSHA1("8ab686eafeb1f44702738c8b0f24f2567c36da6d")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	wantPrefix := object.Prefix{
+		Type: object.TypeBlob,
+		Size: int64(len(content)),
+	}
+	gotHash, err := repo.WriteObject(ctx, wantPrefix, strings.NewReader(content))
+	if gotHash != wantHash || err != nil {
+		t.Errorf("WriteObject(...) = %v, %v; want %v, <nil>", gotHash, err, wantHash)
+	}
+	wantRepo := Map{
+		wantHash: {Type: object.TypeBlob, Data: []byte(content)},
+	}
+	if diff := cmp.Diff(wantRepo, repo); diff != "" {
+		t.Errorf("repo (-want +got):\n%s", diff)
+	}
+
+	gotPrefix, gotReader, err := repo.OpenObject(ctx, wantHash)
+	if gotReader != nil {
+		defer gotReader.Close()
+	}
+	if gotPrefix != wantPrefix || err != nil {
+		t.Errorf("repo.OpenObject(ctx, %v) = %v, _, %v; want %v, _, <nil>",
+			wantHash, gotPrefix, err, wantPrefix)
+	}
+	if gotReader != nil {
+		gotContent, err := io.ReadAll(gotReader)
+		if string(gotContent) != content || err != nil {
+			t.Errorf("io.ReadAll(repo.OpenObject(ctx, %v)) = %q, %v; want %q, <nil>",
+				wantHash, gotContent, err, content)
+		}
+	}
+}
+
+func TestCat(t *testing.T) {
+	repo := make(Map)
+	blobID := repo.Add(Object{
+		Type: object.TypeBlob,
+		Data: []byte("Hello, World!\n"),
+	})
+	treeData, err := (object.Tree{
+		{Name: "hello.txt", Mode: object.ModePlain, ObjectID: blobID},
+	}).MarshalBinary()
+	if err != nil {
+		t.Fatal(err)
+	}
+	treeID := repo.Add(Object{
+		Type: object.TypeTree,
+		Data: treeData,
+	})
+	commitData, err := (&object.Commit{
+		Tree:       treeID,
+		Author:     "Ross Light <ross@zombiezen.com>",
+		AuthorTime: time.Date(2023, time.December, 11, 14, 3, 0, 0, time.FixedZone("PST", -8*60*60)),
+		Committer:  "Ross Light <ross@zombiezen.com>",
+		CommitTime: time.Date(2023, time.December, 11, 14, 3, 0, 0, time.FixedZone("PST", -8*60*60)),
+		Message:    "Initial import",
+	}).MarshalBinary()
+	if err != nil {
+		t.Fatal(err)
+	}
+	commitID := repo.Add(Object{
+		Type: object.TypeCommit,
+		Data: commitData,
+	})
+	badCommitData, err := (&object.Commit{
+		Tree:       blobID,
+		Author:     "Ross Light <ross@zombiezen.com>",
+		AuthorTime: time.Date(2023, time.December, 11, 14, 3, 0, 0, time.FixedZone("PST", -8*60*60)),
+		Committer:  "Ross Light <ross@zombiezen.com>",
+		CommitTime: time.Date(2023, time.December, 11, 14, 3, 0, 0, time.FixedZone("PST", -8*60*60)),
+		Message:    "Initial import",
+	}).MarshalBinary()
+	if err != nil {
+		t.Fatal(err)
+	}
+	badCommitID := repo.Add(Object{
+		Type: object.TypeCommit,
+		Data: badCommitData,
+	})
+	tagData, err := (&object.Tag{
+		ObjectID:   commitID,
+		ObjectType: object.TypeCommit,
+		Tagger:     "Ross Light <ross@zombiezen.com>",
+		Time:       time.Date(2023, time.December, 11, 14, 6, 0, 0, time.FixedZone("PST", -8*60*60)),
+		Name:       "v1",
+		Message:    "First version",
+	}).MarshalBinary()
+	if err != nil {
+		t.Fatal(err)
+	}
+	tagID := repo.Add(Object{
+		Type: object.TypeTag,
+		Data: tagData,
+	})
+
+	tests := []struct {
+		name string
+		id   githash.SHA1
+		want map[object.Type][]byte
+	}{
+		{
+			name: "Blob",
+			id:   blobID,
+			want: map[object.Type][]byte{
+				object.TypeBlob: []byte("Hello, World!\n"),
+			},
+		},
+		{
+			name: "Tree",
+			id:   treeID,
+			want: map[object.Type][]byte{
+				object.TypeTree: treeData,
+			},
+		},
+		{
+			name: "Commit",
+			id:   commitID,
+			want: map[object.Type][]byte{
+				object.TypeTree:   treeData,
+				object.TypeCommit: commitData,
+			},
+		},
+		{
+			name: "Tag",
+			id:   tagID,
+			want: map[object.Type][]byte{
+				object.TypeTree:   treeData,
+				object.TypeCommit: commitData,
+				object.TypeTag:    tagData,
+			},
+		},
+		{
+			name: "BadCommit",
+			id:   badCommitID,
+			want: map[object.Type][]byte{
+				object.TypeCommit: badCommitData,
+			},
+		},
+		{
+			name: "ZeroID",
+			id:   githash.SHA1{},
+			want: nil,
+		},
+	}
+	knownTypes := [...]object.Type{
+		object.TypeBlob, object.TypeTree, object.TypeCommit, object.TypeTag,
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			ctx := context.Background()
+			for _, tp := range knownTypes {
+				buf := new(bytes.Buffer)
+				want, wantsType := test.want[tp]
+				err := Cat(ctx, repo, buf, tp, test.id)
+				if !wantsType {
+					if err == nil {
+						t.Errorf("Cat(ctx, repo, buf, %q, %v) did not return error", tp, test.id)
+					}
+					continue
+				}
+				if err != nil {
+					t.Errorf("Cat(ctx, repo, buf, %q, %v): %v", tp, test.id, err)
+				}
+				if diff := cmp.Diff(want, buf.Bytes()); diff != "" {
+					t.Errorf("after Cat(ctx, repo, buf, %q, %v): buf.Bytes() (-want +got):\n%s",
+						tp, test.id, diff)
+				}
+			}
+		})
+	}
+}
